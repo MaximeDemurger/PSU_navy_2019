d52ec1ffdcc9ae82183bafcee608989e6a680f1e
MaximeDemurger <maxime.demurger@epitech.eu>
2020-03-03 14:31:22 +0100

    trying to unsegfault

--- src/signal/signal_connexion.c
+++ src/signal/signal_connexion.c
@@ -18,8 +18,6 @@ int global_var;
 void handler(int signal, siginfo_t *info, void *x)
 {
     my_putstr("\nenemy connected\n\n");
-    //utils->pid->enemy_pid = info->si_pid;
-    usleep(5000);
     global_var = info->si_pid;
 }
 
@@ -44,9 +42,8 @@ int serveur(utils_t *utils)
 
 int enemy_pos(char **argv, utils_t *utils)
 {
-    if (!argv) {
+    if (!argv)
         return 84;
-    }
     utils->pid->my_pid = getpid();
     utils->pid->enemy_pid = my_atoi(argv[1]);
     if (my_putstr("my_pid: ") == 84
